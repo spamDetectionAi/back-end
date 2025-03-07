c9c763512e4f3e563da66e028f702127bdd4262c
TsioryRasoloarijaona <hei.tsiory@gmail.com>
2025-03-07 13:16:41 +0000

    feat : project initialisation

---
+++ src/main/java/com/tsix_hack/spam_ai_detection/SpamAiDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.tsix_hack.spam_ai_detection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpamAiDetectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpamAiDetectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tsix_hack/spam_ai_detection/SpamAiDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsix_hack.spam_ai_detection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpamAiDetectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
